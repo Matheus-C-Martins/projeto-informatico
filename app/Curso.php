528a68a803659f1fe49d15324de5efd10493c6bd
Ivan <ivangonzagasilva@live.com.pt>
2020-05-05 15:14:59 +0100

    [MIGRATIONS] - Tabela de Inquéritos e tabela de Respostas de Inquéritos

--- app/Curso.php
+++ app/Curso.php
@@ -5,6 +5,8 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Curso extends Model {
+    public $timestamps = FALSE;
+    
     protected $fillable = [
         'abreviatura', 'nome', 'tipo', 'semestres', 'ETCS', 'vagas', 'contato', 'objetivos',
     ];
